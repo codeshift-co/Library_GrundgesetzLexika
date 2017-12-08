2d16855751ad185aa5a2b26d0efa28de3f0a8901
3PC\lfranken <lfranken@3pc.de>
2017-12-08 11:11:18 +0100

    [UPDATE] added ParallaxRecyclerViewOnScrollListener

---
+++ LibraryModule/src/main/java/com/example/lfranken/grundgesetzlexikalibrary/custom_views/ParallaxRecylcerViewOnScrollListener.java
@@ -0,0 +1,18 @@
+package com.example.lfranken.grundgesetzlexikalibrary.custom_views;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ParallaxRecylcerViewOnScrollListener extends RecyclerView.OnScrollListener {
+
+    private static final float PARALLAX_FACTOR = 2.5f;
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        View view = recyclerView.getChildAt(0);
+        if (view != null && recyclerView.getChildAdapterPosition(view) == 0){
+            view.setTranslationY(((float)-view.getTop()) / PARALLAX_FACTOR);
+        }
+    }
+}
