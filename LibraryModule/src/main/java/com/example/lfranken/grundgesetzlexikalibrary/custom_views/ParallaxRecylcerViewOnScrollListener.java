3596233794ff0620fa89c8b4190661a103b439b3
3PC\lfranken <lfranken@3pc.de>
2017-12-08 16:15:55 +0100

    [UPDATE] updated funcitonallity of ParallaxRecyclerOnScrollChangeListener

--- LibraryModule/src/main/java/com/example/lfranken/grundgesetzlexikalibrary/custom_views/ParallaxRecylcerViewOnScrollListener.java
+++ LibraryModule/src/main/java/com/example/lfranken/grundgesetzlexikalibrary/custom_views/ParallaxRecylcerViewOnScrollListener.java
@@ -7,10 +7,22 @@ public class ParallaxRecylcerViewOnScrollListener extends RecyclerView.OnScrollL
 
     private static final float PARALLAX_FACTOR = 2.5f;
 
+    private boolean usingOutsideView;
+    private View view;
+
+    public ParallaxRecylcerViewOnScrollListener(){
+        usingOutsideView = false;
+    }
+
+    public ParallaxRecylcerViewOnScrollListener(View view){
+        this.view = view;
+        usingOutsideView = true;
+    }
+
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
-        View view = recyclerView.getChildAt(0);
+        View view = (usingOutsideView) ? this.view : recyclerView.getChildAt(0);
         if (view != null && recyclerView.getChildAdapterPosition(view) == 0){
             view.setTranslationY(((float)-view.getTop()) / PARALLAX_FACTOR);
         }
